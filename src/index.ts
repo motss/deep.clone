26e266a1fd6aa0d8fb9173558ae7a501073cb065
Rong Sen Ng <wes.ngrongsen@gmail.com>
2017-12-03 22:32:40 +0800

    :boom: Fixes #17 (#19)
    * :fire: Minor cleanup
    
    * :fire: Now in TypeScript with better test scripts
    
    * :memo: Updated README.md
    
    * :memo: Updated README.md

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+// @ts-check
+
+export declare interface DeepCloneOptions {
+  absolute?: boolean;
+}
+
+/** Import project dependencies */
+import * as lodashCloneDeep from 'lodash.clonedeep';
+
+export function deepCloneSync(target: any, options?: DeepCloneOptions) {
+  if (typeof target === 'undefined') {
+    throw new TypeError('target is undefined');
+  }
+
+  if (
+    typeof options !== 'undefined'
+      && {}.hasOwnProperty.call(options, 'absolute')
+      && options.absolute === true
+  ) {
+    return lodashCloneDeep(target);
+  }
+
+  return JSON.parse(JSON.stringify(target));
+}
+
+export async function deepClone(target: any, options?: DeepCloneOptions) {
+  try {
+    return deepCloneSync(target, options);
+  } catch (e) {
+    throw e;
+  }
+}
+
+export default deepClone;
